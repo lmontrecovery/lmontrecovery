de55ac303bd593f91a268de7dfb0f448225085d2
lmontrecovery <52107864+lmontrecovery@users.noreply.github.com>
2019-06-22 21:59:06 +0600

    3434
    3434

---
+++ imsearch.php
@@ -0,0 +1,120 @@
+<?php require_once("res/x5engine.php"); ?>
+<!DOCTYPE html><!-- HTML5 -->
+<html prefix="og: http://ogp.me/ns#" lang="ru-RU" dir="ltr">
+	<head>
+		<title>Поиск - LMONT.dieNET</title>
+		<meta charset="utf-8" />
+		<!--[if IE]><meta http-equiv="ImageToolbar" content="False" /><![endif]-->
+		<meta name="author" content="lmontCORP" />
+		<meta name="generator" content="Incomedia WebSite X5 Professional 14.0.3.1 - www.websitex5.com" />
+		<meta property="og:locale" content="ru" />
+		<meta property="og:type" content="website" />
+		<meta property="og:url" content="http://lmontrecovery.cf/imsearch.php" />
+		<meta property="og:title" content="Поиск" />
+		<meta property="og:site_name" content="LMONT.dieNET" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		
+		<link rel="stylesheet" type="text/css" href="style/reset.css?14-0-3-1" media="screen,print" />
+		<link rel="stylesheet" type="text/css" href="style/print.css?14-0-3-1" media="print" />
+		<link rel="stylesheet" type="text/css" href="style/style.css?14-0-3-1" media="screen,print" />
+		<link rel="stylesheet" type="text/css" href="style/template.css?14-0-3-1" media="screen" />
+		<link rel="stylesheet" type="text/css" href="appsresources/css/style.css" media="screen, print" />
+		<link rel="stylesheet" type="text/css" href="imHeader_pluginAppObj_03/css/custom.css" media="screen, print" /><link rel="stylesheet" type="text/css" href="imHeader_pluginAppObj_04/css/custom.css" media="screen, print" /><link rel="stylesheet" type="text/css" href="imHeader_pluginAppObj_05/css/custom.css" media="screen, print" /><link rel="stylesheet" type="text/css" href="imHeader_pluginAppObj_06/css/custom.css" media="screen, print" /><link rel="stylesheet" type="text/css" href="imHeader_pluginAppObj_07/css/custom.css" media="screen, print" />
+		<link rel="stylesheet" type="text/css" href="pcss/imsearch.css?14-0-3-1-636966753639809198" media="screen,print" />
+		<script type="text/javascript" src="res/jquery.js?14-0-3-1"></script>
+		<script type="text/javascript" src="res/x5engine.js?14-0-3-1" data-files-version="14-0-3-1"></script>
+		<script type="text/javascript" src="appsresources/js/main.js"></script>
+		<script type="text/javascript">
+			window.onload = function(){ checkBrowserCompatibility('Ваш браузер не поддерживает функции, требуемые для визуализации этого Сайта.','Возможно, Ваш браузер не поддерживает функции, требуемые для визуализации этого Сайта.','[1]Обновите Ваш браузер[/1] или [2]продолжите процедуру [/2].','http://outdatedbrowser.com/'); };
+			x5engine.utils.currentPagePath = 'imsearch.php';
+			x5engine.boot.push(function () { x5engine.utils.imCodeProtection('lmontCORP'); });
+			x5engine.boot.push(function () { x5engine.imPageToTop.initializeButton({}); });
+		</script>
+		<link rel="icon" href="favicon.ico?14-0-3-1-636966753639779196" type="image/vnd.microsoft.icon" />
+	</head>
+	<body>
+		<div id="imPageExtContainer">
+			<div id="imPageIntContainer">
+				<div id="imHeaderBg"></div>
+				<div id="imFooterBg"></div>
+				<div id="imPage">
+					<div id="imHeader">
+						<h1 class="imHidden">Поиск - LMONT.dieNET</h1>
+						<div id="imHeaderObjects"><div id="imHeader_imObjectImage_01_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_1" class="" > <div id="imHeader_imCellStyleGraphics_1"></div><div id="imHeader_imCellStyle_1" ><img id="imHeader_imObjectImage_01" src="images/11111.png" title="" alt="" /></div></div></div><div id="imHeader_imObjectImage_02_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_2" class="" > <div id="imHeader_imCellStyleGraphics_2"></div><div id="imHeader_imCellStyle_2" ><img id="imHeader_imObjectImage_02" src="images/222.png" title="" alt="" /></div></div></div><div id="imHeader_pluginAppObj_03_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_3" class="" > <div id="imHeader_imCellStyleGraphics_3"></div><div id="imHeader_imCellStyle_3" ><div id="imHeader_pluginAppObj_03">
+
+	  <div class="cd-intro"><p class="cd-headline clip is-full-width"><span>  die.NET:&nbsp;</span><span class="cd-words-wrapper"><b class="is-visible">Восстановление сообщества</b><b>Community recovery</b><b>Erholung in der Gemeinschaft</b><b>Recupero comunitario</b><b>社區恢復</b><b>커뮤니티 복구</b><b>コミュニティの復興</b></span>&nbsp;<span>  </span></p></div>
+   </div></div></div></div><div id="imHeader_pluginAppObj_04_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_4" class="" > <div id="imHeader_imCellStyleGraphics_4"></div><div id="imHeader_imCellStyle_4" ><div id="imHeader_pluginAppObj_04">
+
+	  <div class="cd-intro"><p class="cd-headline clip is-full-width"><span>  die.NET:&nbsp;</span><span class="cd-words-wrapper"><b class="is-visible">Восстановление сообщества</b><b>Community recovery</b><b>Erholung in der Gemeinschaft</b><b>Recupero comunitario</b><b>社區恢復</b><b>커뮤니티 복구</b><b>コミュニティの復興</b></span></p></div>
+   </div></div></div></div><div id="imHeader_pluginAppObj_05_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_5" class="" > <div id="imHeader_imCellStyleGraphics_5"></div><div id="imHeader_imCellStyle_5" ><div id="imHeader_pluginAppObj_05">
+
+	  <div class="cd-intro"><p class="cd-headline clip is-full-width"><span>  die.NET:&nbsp;</span><span class="cd-words-wrapper"><b class="is-visible">Восстановление сообщества</b><b>Community recovery</b><b>Erholung in der Gemeinschaft</b><b>Recupero comunitario</b><b>社區恢復</b><b>커뮤니티 복구</b><b>コミュニティの復興</b></span></p></div>
+   </div></div></div></div><div id="imHeader_pluginAppObj_06_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_6" class="" > <div id="imHeader_imCellStyleGraphics_6"></div><div id="imHeader_imCellStyle_6" ><div id="imHeader_pluginAppObj_06">
+
+	  <div class="cd-intro"><p class="cd-headline clip is-full-width"><span>  die.NET:&nbsp;</span><span class="cd-words-wrapper"><b class="is-visible">Восстановление сообщества</b><b>Community recovery</b><b>Erholung in der Gemeinschaft</b><b>Recupero comunitario</b><b>社區恢復</b><b>커뮤니티 복구</b><b>コミュニティの復興</b></span></p></div>
+   </div></div></div></div><div id="imHeader_pluginAppObj_07_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_7" class="" > <div id="imHeader_imCellStyleGraphics_7"></div><div id="imHeader_imCellStyle_7" ><div id="imHeader_pluginAppObj_07">
+
+	  <div class="cd-intro"><p class="cd-headline clip is-full-width"><span>  die.NET:&nbsp;</span><span class="cd-words-wrapper"><b class="is-visible">Восстановление сообщества</b><b>Community recovery</b><b>Erholung in der Gemeinschaft</b><b>Recupero comunitario</b><b>社區恢復</b><b>커뮤니티 복구</b><b>コミュニティの復興</b></span></p></div>
+   </div></div></div></div></div>
+					</div>
+					<div id="imStickyBarContainer">
+						<div id="imStickyBarGraphics"></div>
+						<div id="imStickyBar">
+							<div id="imStickyBarObjects"><div id="imStickyBar_imMenuObject_01_wrapper" class="template-object-wrapper"><div id="imStickyBar_imCell_1" class="" > <div id="imStickyBar_imCellStyleGraphics_1"></div><div id="imStickyBar_imCellStyle_1" ><div id="imStickyBar_imMenuObject_01"><div class="hamburger-button hamburger-component"><div><div><div class="hamburger-bar"></div><div class="hamburger-bar"></div><div class="hamburger-bar"></div></div></div></div><div class="hamburger-menu-background-container hamburger-component">
+	<div class="hamburger-menu-background menu-mobile menu-mobile-animated hidden">
+		<div class="hamburger-menu-close-button"><span>&times;</span></div>
+	</div>
+</div>
+<ul class="menu-mobile-animated hidden" data-flip-direction="1">
+	<li class=" imPage" data-link-paths="/index.html,/">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="index.html">
+HOME		</a>
+</div>
+</div>
+	</li></ul></div><script type="text/javascript">
+x5engine.boot.push(function(){x5engine.initMenu('imStickyBar_imMenuObject_01',1000)});
+$(function () {$('#imStickyBar_imMenuObject_01 ul li').each(function () {    var $this = $(this), timeout = 0;    $this.on('mouseenter', function () {        if($(this).parents('#imStickyBar_imMenuObject_01-menu-opened').length > 0) return;         clearTimeout(timeout);        setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeIn(); }, 250);    }).on('mouseleave', function () {        if($(this).parents('#imStickyBar_imMenuObject_01-menu-opened').length > 0) return;         timeout = setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeOut(); }, 250);    });});});
+
+</script>
+</div></div></div><div id="imStickyBar_imObjectTitle_02_wrapper" class="template-object-wrapper"><div id="imStickyBar_imCell_2" class="" > <div id="imStickyBar_imCellStyleGraphics_2"></div><div id="imStickyBar_imCellStyle_2" ><div id="imStickyBar_imObjectTitle_02"><span id ="imStickyBar_imObjectTitle_02_text" >serigraph</span > </div></div></div></div></div>
+						</div>
+					</div>
+					<a class="imHidden" href="#imGoToCont" title="Заголовок главного меню">Перейти к контенту</a>
+					<div id="imSideBarContainer">
+						<div id="imSideBarGraphics"></div>
+						<div id="imSideBar">
+							<div id="imSideBarObjects"></div>
+						</div>
+					</div>
+					<div id="imContentContainer">
+						<div id="imContentGraphics"></div>
+						<div id="imContent">
+							<a id="imGoToCont"></a>
+				<div id="imSearchPage">
+				<h2 id="imPgTitle">Результаты поиска</h2>
+				<?php
+				$search = new imSearch();
+				$keys = isset($_GET['search']) ? @htmlspecialchars($_GET['search']) : "";
+				$page = isset($_GET['page']) ? @htmlspecialchars($_GET['page']) : 0;
+				$type = isset($_GET['type']) ? @htmlspecialchars($_GET['type']) : "pages"; ?>
+				<div class="searchPageContainer">
+				<?php echo $search->search($keys, $page, $type); ?>
+				</div>
+				</div>
+				
+							<div class="imClear"></div>
+						</div>
+					</div>
+					<div id="imFooter">
+						<div id="imFooterObjects"></div>
+					</div>
+				</div>
+				<span class="imHidden"><a href="#imGoToCont" title="Прочесть эту страницу заново">Назад к содержимому</a></span>
+			</div>
+		</div>
+		
+		<noscript class="imNoScript"><div class="alert alert-red">Для использования этого сайта необходимо включить JavaScript.</div></noscript>
+	</body>
+</html>
